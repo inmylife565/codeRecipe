a0767484bd8a55eec62ff3998ab543084b668216
inmylife565 <63580985+inmylife565@users.noreply.github.com>
2021-12-09 18:52:44 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+{
+	const searchWord = document.getElementById('searchWord');
+	const list = document.querySelectorAll('li');
+	const headline = document.querySelectorAll('h4');
+
+	searchWord.focus();
+
+	searchWord.addEventListener('keyup', () => {
+		list.forEach((element) => {
+			const sw = searchWord.value;
+			if (!sw || sw === '') {
+				element.classList.remove('hide');
+				headline.forEach((el) => {
+					el.classList.remove('hide');
+				});
+				return;
+			}
+			const listText = element.innerText;
+
+			if (listText.includes(sw)) {
+				element.classList.remove('hide');
+			} else {
+				element.classList.add('hide');
+				headline.forEach((el) => {
+					el.classList.add('hide');
+				});
+			}
+		});
+	});
+
+	const crl = document.getElementById('clear');
+	crl.addEventListener('click', () => {
+		location.reload();
+	});
+
+}
